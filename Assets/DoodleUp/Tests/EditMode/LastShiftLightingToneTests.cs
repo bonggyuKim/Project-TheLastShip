8fdf2d8436a776793c8db9e8f1ca4652391babbe
bonggyuKim <50454395+bonggyuKim@users.noreply.github.com>
2026-08-14 17:02:30 +0900

    #070b4fc2-c389-40d0-92c4-c81967f31a45 [LAST SHIFT] 공통 조명·중간명도 패스 - 무정보 암면 복원

--- Assets/DoodleUp/Tests/EditMode/LastShiftLightingToneTests.cs
+++ Assets/DoodleUp/Tests/EditMode/LastShiftLightingToneTests.cs
@@ -1,5 +1,6 @@
 using DoodleUp.Editor;
 using NUnit.Framework;
+using UnityEditor.SceneManagement;
 using UnityEngine;
 
 namespace DoodleUp.Tests.EditMode
@@ -34,6 +35,26 @@ namespace DoodleUp.Tests.EditMode
             Assert.That(directional.intensity, Is.GreaterThanOrEqualTo(0.4f));
             Assert.That(directional.color.r - directional.color.b, Is.LessThanOrEqualTo(0.1f),
                 "형태 보조광에 강한 피치 캐스트가 남으면 구역색과 소품색이 왜곡된다.");
+        }
+
+        [TestCase("Assets/Scenes/LAST_SHIFT_SOLO.unity")]
+        [TestCase("Assets/Scenes/LAST_SHIFT_SP02A_NETWORK.unity")]
+        public void ShippedScenesKeepSharedMidtonePass(string scenePath)
+        {
+            EditorSceneManager.OpenScene(scenePath, OpenSceneMode.Single);
+
+            var ambient = RenderSettings.ambientLight;
+            Assert.That(RenderSettings.ambientMode,
+                Is.EqualTo(UnityEngine.Rendering.AmbientMode.Flat));
+            Assert.That(ambient.grayscale, Is.GreaterThanOrEqualTo(0.19f), scenePath);
+            Assert.That(ambient.r - ambient.b, Is.InRange(0f, 0.025f), scenePath);
+
+            var directionalObject = GameObject.Find("Directional Light");
+            Assert.That(directionalObject, Is.Not.Null, scenePath);
+            var directional = directionalObject.GetComponent<Light>();
+            Assert.That(directional.intensity, Is.GreaterThanOrEqualTo(0.4f), scenePath);
+            Assert.That(directional.color.r - directional.color.b,
+                Is.LessThanOrEqualTo(0.1f), scenePath);
         }
     }
 }
